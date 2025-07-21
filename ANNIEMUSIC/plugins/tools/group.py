700018ea99d45a25721499988fa864c89ef268ce
ᴍʀ ᴘᴇʀғᴇᴄᴛ x ᴛɪᴛᴀɴ (◕‿◕) <132195434+Mrperfectxd@users.noreply.github.com>
2025-07-21 06:45:27 +0530

    group.py

--- ANNIEMUSIC/plugins/tools/group.py
+++ ANNIEMUSIC/plugins/tools/group.py
@@ -10,25 +10,6 @@ async def on_voice_chat_started(_, message: Message):
     await message.reply_text("🎙 **ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ʜᴀs sᴛᴀʀᴛᴇᴅ!**")
 
 
-@app.on_message(filters.video_chat_ended)
-async def on_voice_chat_ended(_, message: Message):
-    await message.reply_text("🔕 **ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ.**")
-
-
-@app.on_message(filters.video_chat_members_invited)
-async def on_voice_chat_members_invited(_, message: Message):
-    inviter = message.from_user.mention if message.from_user else "Someone"
-    invited_list = []
-
-    for user in message.video_chat_members_invited.users:
-        try:
-            invited_list.append(f"[{user.first_name}](tg://user?id={user.id})")
-        except:
-            continue
-
-    if invited_list:
-        users = ", ".join(invited_list)
-        await message.reply_text(f"👥 {inviter} ɪɴᴠɪᴛᴇᴅ {users} ᴛᴏ ᴛʜᴇ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ. 😉")
 
 
 @app.on_message(filters.command("leavegroup") & filters.user(OWNER_ID))
